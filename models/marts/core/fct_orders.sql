854f3b1c2c2b6a31e97a6947e4a2497391c2e9f3
woghirenedokpaigbe@gmail.com <woghirenedokpaigbe@gmail.com>
2022-05-02 22:40:07 +0000

    github test

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,24 @@
+with payments  as (
+
+    select * from {{ ref('stg_payments')}}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+fct_orders as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        payments.amount ,
+        payments.status
+    from orders
+    join payments using (order_id)
+
+)
+
+select * from fct_orders
